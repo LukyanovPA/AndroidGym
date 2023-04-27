c471e867f79d1d913c3c0b7f28a89d667d941f50
pavel <pavel.lukyanovv@gmail.com>
2023-04-27 04:33:49 +0300

    refactor: same logics

--- app/src/main/java/com/pavellukyanov/androidgym/ui/feature/categories/CategoriesScreen.kt
+++ app/src/main/java/com/pavellukyanov/androidgym/ui/feature/categories/CategoriesScreen.kt
@@ -1,6 +1,5 @@
 package com.pavellukyanov.androidgym.ui.feature.categories
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,19 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.pavellukyanov.androidgym.helper.ext.asUiState
@@ -75,10 +69,12 @@ fun CategoriesScreenContent(
 //                    .clickable { onAction(CategoriesAction.GoToCategory(category.id)) },
 //                contentAlignment = Alignment.Center
 //            ) {
-                Text(
-                    text = category.name,
-                    modifier = Modifier.fillMaxSize()
-                )
+            Text(
+                text = category.name,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable { onAction(CategoriesAction.GoToCategory(category.id)) }
+            )
 //            }
         }
     }
